33c06ea01c4d714638818063826735cb0f210762
Pratyush Sharma <pratyushsharma258@gmail.com>
2024-12-18 09:13:43 +0000

    Completed basic audio visualization

--- main.cpp
+++ main.cpp
@@ -1,23 +1,76 @@
 #include <stdlib.h>
-#include <iostream>
+#include <stdio.h>
+#include <cstring>
 #include <portaudio.h>
 
-using namespace std;
+#define SAMPLE_RATE 44100.000
+#define FRAMES_PER_BUFFER 512
 
-static void checkError(PaError err)
+static void checkErr(PaError err)
 {
     if (err != paNoError)
     {
-        cout << "Error: " << Pa_GetErrorText(err) << endl;
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
         exit(EXIT_FAILURE);
     }
+}
+
+static inline float max(float a, float b)
+{
+    return a > b ? a : b;
+}
+
+static int patestCallback(
+    const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer,
+    const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags,
+    void *userData)
+{
+    float *in = (float *)inputBuffer;
+    (void)outputBuffer;
+
+    int dispSize = 100;
+    printf("\r");
+
+    float vol_l = 0;
+    float vol_r = 0;
+
+    for (unsigned long i = 0; i < framesPerBuffer * 2; i += 2)
+    {
+        vol_l = max(vol_l, std::abs(in[i]));
+        vol_r = max(vol_r, std::abs(in[i + 1]));
+    }
+
+    for (int i = 0; i < dispSize; i++)
+    {
+        float barProportion = i / (float)dispSize;
+        if (barProportion <= vol_l && barProportion <= vol_r)
+        {
+            printf("█");
+        }
+        else if (barProportion <= vol_l)
+        {
+            printf("▀");
+        }
+        else if (barProportion <= vol_r)
+        {
+            printf("▄");
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+
+    fflush(stdout);
+
+    return 0;
 }
 
 int main()
 {
     PaError err;
     err = Pa_Initialize();
-    checkError(err);
+    checkErr(err);
 
     int numDevices = Pa_GetDeviceCount();
     printf("Number of devices: %d\n", numDevices);
@@ -44,38 +97,50 @@ int main()
         printf("  defaultSampleRate: %f\n", deviceInfo->defaultSampleRate);
     }
 
-    int device = 0;
+    int device = 11;
+
+    PaStreamParameters inputParameters;
+    PaStreamParameters outputParameters;
+
+    memset(&inputParameters, 0, sizeof(inputParameters));
+    inputParameters.channelCount = 2;
+    inputParameters.device = device;
+    inputParameters.hostApiSpecificStreamInfo = NULL;
+    inputParameters.sampleFormat = paFloat32;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowInputLatency;
+
+    memset(&outputParameters, 0, sizeof(outputParameters));
+    outputParameters.channelCount = 2;
+    outputParameters.device = device;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+    outputParameters.sampleFormat = paFloat32;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowInputLatency;
 
     PaStream *stream;
-    PaStreamParameters inputParameters, outputParameters;
-    inputParameters.device = device;
-    inputParameters.channelCount = 1;
-    inputParameters.sampleFormat = paInt16;
-    inputParameters.suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowInputLatency;
-    inputParameters.hostApiSpecificStreamInfo = NULL;
-
-    outputParameters.device = device;
-    outputParameters.channelCount = 1;
-    outputParameters.sampleFormat = paInt16;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowOutputLatency;
-    outputParameters.hostApiSpecificStreamInfo = NULL;
-
-    err = Pa_OpenStream(&stream, &inputParameters, &outputParameters, 44100, 256, paClipOff, NULL, NULL);
-    checkError(err);
+    err = Pa_OpenStream(
+        &stream,
+        &inputParameters,
+        &outputParameters,
+        SAMPLE_RATE,
+        FRAMES_PER_BUFFER,
+        paNoFlag,
+        patestCallback,
+        NULL);
+    checkErr(err);
 
     err = Pa_StartStream(stream);
-    checkError(err);
+    checkErr(err);
 
-    printf("Press Enter to stop the stream.\n");
-    getchar();
+    Pa_Sleep(10 * 1000);
 
     err = Pa_StopStream(stream);
-    checkError(err);
+    checkErr(err);
 
     err = Pa_CloseStream(stream);
-    checkError(err);
+    checkErr(err);
 
-    Pa_Terminate();
+    err = Pa_Terminate();
+    checkErr(err);
 
     return EXIT_SUCCESS;
 }
